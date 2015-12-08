dec8442123fa24c031ad3816c6595924a20d181b
cathayandy <wzm_andy@126.com>
2015-12-08 20:04:04 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var path = require('path');
+
+var app = express();
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+
+
+app.use('/css', express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
