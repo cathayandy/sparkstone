f152bc8b0018273db8bdc900a3ade080440c57d1
cathayandy <wzm_andy@126.com>
2015-12-08 22:00:04 +0800

    listen 80

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'hbs');
 app.use('/css', express.static(__dirname + '/public/css'));
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(3000, function () {
+var server = app.listen(80, function () {
   var host = server.address().address;
   var port = server.address().port;
 
